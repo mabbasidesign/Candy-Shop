3f8951a5e1ee9554067ba2c8e5dbe78f2487aff8
Mohsen <mabbasidesign@yahoo.com>
2019-12-29 16:11:28 -0500

    Seed-Database

--- CandyShop/Data/Migrations/20191229210949_Seed-Database.Designer.cs
+++ CandyShop/Data/Migrations/20191229210949_Seed-Database.Designer.cs
@@ -4,14 +4,16 @@ using CandyShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandyShop.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191229210949_Seed-Database")]
+    partial class SeedDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
